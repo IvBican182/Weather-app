401e66cbda6320496439e0f38425951027caa3f4
IvBican182 <ibicanic@live.com>
2024-01-31 10:38:18 +0100

    modifying functions for getting location and city

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
-import { getWeather, getCurrentLocationWeather } from "./get_weather";
+import { getLocationWeather, getCurrentCity } from "./get_weather";
 
 const searchBtn = document.getElementById("search-btn");
-searchBtn.addEventListener("click", getWeather);
+searchBtn.addEventListener("click", () => {
+    const city = document.getElementById("city").value; 
+    
+    if (!city) {
+        alert('Please enter a city');
+        return
+    }
+    getLocationWeather(city);
+});
 
 const locationBtn = document.getElementById("location-btn");
-locationBtn.addEventListener("click",getCurrentLocationWeather);
+locationBtn.addEventListener("click",  async () => {
+    const currentLocation = await getCurrentCity();
+    getLocationWeather(currentLocation);
+});
 
 
-
-console.log("test");
