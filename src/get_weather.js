c45e25b13b9246109d31de76c1cb047bf3c98319
IvBican182 <ibicanic@live.com>
2024-01-31 10:56:27 +0100

    finishing final changes

--- src/get_weather.js
+++ src/get_weather.js
@@ -1,38 +1,38 @@
 import { displayWeather, displayDaysForecast, displayHourlyForecast } from "./display_weather";
 
-export const API_BASE_URL = 'http://api.openweathermap.org/';
-export const apiKey = '4e892c19e0b10a90b88b86a8fcfb060e';
+export const API_BASE_URL = 'http://api.openweathermap.org/'; // base url
+export const apiKey = '4e892c19e0b10a90b88b86a8fcfb060e'; //storing api key
 
 
-
+//returning a promise, if resolved (user allows us to use location) will store position later
 function getPosition() {
     return new Promise((res, rej) => {
         navigator.geolocation.getCurrentPosition(res, rej);
     });
 }
 
-export async function getCurrentCity()
+export async function getCurrentCity() 
 {
     const position = await getPosition();
     const { latitude, longitude } = position.coords;
-    const REVERSE_GEOCODING_URL = `${API_BASE_URL}geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${apiKey}`;
+    const REVERSE_GEOCODING_URL = `${API_BASE_URL}geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${apiKey}`; //coordinates geo api
     const city = 
         await fetch(REVERSE_GEOCODING_URL)
             .then(response => response.json())
             .then(data => {
-                const currentLocation = data[0].name; //retrieving current location name 
+                const currentLocation = data[0].name; //retrieving current location name from output object
                 const currentLow = currentLocation.toLowerCase();
                 const cityName = currentLow.toString();
 
-                return cityName;
+                return cityName; //storing retrieved location name, making it equal to city
             });
 
-    return city;
+    return city; //returning city for later use (getting weather)
 }
 
 export function getLocationWeather(cityName)
 {
-    const currentWeatherUrl = `${API_BASE_URL}data/2.5/weather?q=${cityName}&appid=${apiKey}`; //passing location name to URL
+    const currentWeatherUrl = `${API_BASE_URL}data/2.5/weather?q=${cityName}&appid=${apiKey}`;
     const forecastUrl = `${API_BASE_URL}data/2.5/forecast?q=${cityName}&appid=${apiKey}`;
     
     fetch(currentWeatherUrl)
--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import { getLocationWeather, getCurrentCity } from "./get_weather";
 
-const searchBtn = document.getElementById("search-btn");
+const searchBtn = document.getElementById("search-btn"); 
 searchBtn.addEventListener("click", () => {
-    const city = document.getElementById("city").value; 
+    const city = document.getElementById("city").value; //getting the value from user input
     
     if (!city) {
         alert('Please enter a city');
         return
     }
-    getLocationWeather(city);
+    getLocationWeather(city); //calling the weather function based from city name from users input
 });
 
-const locationBtn = document.getElementById("location-btn");
+const locationBtn = document.getElementById("location-btn"); 
 locationBtn.addEventListener("click",  async () => {
-    const currentLocation = await getCurrentCity();
-    getLocationWeather(currentLocation);
+    const currentLocation = await getCurrentCity(); //making the city variable from function equal to currentLocation variable / ready to use for getting weather
+    getLocationWeather(currentLocation); //calling the weather function based on users current location
 });
 
 
